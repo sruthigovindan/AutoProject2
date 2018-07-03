5507fd75dca3bb3c434bfaaf101b6cc2f98e2f75
sruthigovindan <sgovindan@in.imshealth.com>
2018-07-03 15:33:52 +0530

    Added variable k and done with some changes in j

--- AutoProject2/src/Test1.java
+++ AutoProject2/src/Test1.java
@@ -7,9 +7,12 @@ public class Test1 {
 		for (i=1;i<100;i++)
 		{
 			System.out.println("i..."+i);
-			int j=1+10;
+			int j=i+15;
 			System.out.println("j..."+j);
+			//Adding new changes to dev branch
 			
+			int k=j+15;
+			System.out.println("k...");
 			
 		}
 	}
