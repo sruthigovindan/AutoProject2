f857426fb373124c28c453a90e7cfb4340e79f37
sruthigovindan <sgovindan@in.imshealth.com>
2018-07-03 15:36:05 +0530

    updated autoproject2

--- AutoProject2/src/Test1.java
+++ AutoProject2/src/Test1.java
@@ -7,9 +7,13 @@ public class Test1 {
 		for (i=1;i<100;i++)
 		{
 			System.out.println("i..."+i);
-			int j=1+10;
+			int j=i+10;
 			System.out.println("j..."+j);
 			
+			for (int k=0;k<100;k++)
+			{
+				System.out.println("j..."+j);
+			}
 			
 		}
 	}
