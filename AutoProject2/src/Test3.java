dafaf0a85557acbbf2053299824a0908705a5386
sruthigovindan <40688445+sruthigovindan@users.noreply.github.com>
2018-07-04 14:03:10 +0530

    Create Test3.java

---
+++ AutoProject2/src/Test3.java
@@ -0,0 +1,18 @@
+public class Test3{
+
+	public static void main()
+	{
+		int i=0;
+    int j=0;
+		for (i=1;i<100;i++)
+		{
+			
+
+			int k=j+1098;
+			
+			System.out.println("k...");
+
+			
+		}
+	}
+}
